2e97d0926d5e36a5116e531be81b6b3a6a416b8e
shybash-shaik <shybash.shaik@Minfytech.com>
2025-09-10 12:26:27 +0530

    initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,26 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
     },
-    plugins: { js },
-    extends: ["js/recommended", "plugin:prettier/recommended"],
+    plugins: {
+      js,
+    },
+    extends: ['js/recommended'],
     rules: {
-      semi: ["error", "always"],
-      "no-unused-vars": "warn",
-      quotes: ["error", "single"]
+      semi: ['error', 'always'],
+      'no-unused-vars': 'warn',
+      quotes: ['error', 'single'],
     },
   },
 ]);
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
-const app=express();
+const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('hey there from backend');
+});
 
-app.get('/',(req,res)=>{
-    res.send("hey there from backend");
-})
+const PORT = process.env.PORT || 5000;
 
-
-const PORT=process.env.PORT ||5000;
-
-app.listen(PORT,()=>{
-    console.log(`server running on port $(PORT)`);
+app.listen(PORT, () => {
+  console.log('server running on port $(PORT)');
 });
